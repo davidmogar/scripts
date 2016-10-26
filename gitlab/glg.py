57c8c2a417781aa8dcbf18d7ff3e41a93ba4298a
David Moreno García <david.mogar@cern.ch>
2016-10-26 17:01:06 +0200

    Add script to fetch from a GitLab repository

---
+++ gitlab/glg.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python
+
+import argparse
+import gitlab
+import os, os.path
+
+from subprocess import check_call, CalledProcessError
+
+parser = argparse.ArgumentParser(description='Fetch git projects.')
+parser.add_argument('name', metavar='NAME', type=str, help='Project partial name')
+parser.add_argument('--host', type=str, help='Gitlab host')
+parser.add_argument('--token', type=str, help='User token')
+
+args = parser.parse_args()
+
+def create_config_file(host, token):
+  home = os.path.expanduser('~')
+  with open(os.path.join(home, '.python-gitlab.cfg'), 'w+') as file:
+    file.write(
+"""[global]
+default = gitlab
+ssl_verify = true
+timeout = 5
+
+[gitlab]
+url = """ + host + """
+private_token = """ + token)
+
+def fetch_repositories():
+  gl = gitlab.Gitlab.from_config('gitlab', ['~/.python-gitlab.cfg'])
+  gl.auth()
+
+  projects = gl.projects.search(args.name)
+
+  if projects:
+    repos = len(projects)
+    errors = 0
+
+    for project in projects:
+      print("Fetching %s..." % project.name)
+      try:
+        check_call(['git', 'clone', project.ssh_url_to_repo])
+      except CalledProcessError:
+        errors += 1
+
+    print("%d project(s) downloaded out of %d" % (repos - errors, repos))
+
+if __name__ == '__main__':
+  if args.host and args.token:
+    create_config_file(args.host, args.token)
+
+  fetch_repositories()
